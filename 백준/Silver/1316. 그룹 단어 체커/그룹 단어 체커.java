c486b5ae5a1b5dc47139adf6bf68abaeecf17596
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-01-07 18:27:47 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws  Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int count  = 0;
+
+        for(int  i = 0; i  < n; i++){
+            if(checkStr(br.readLine())){
+                count++;
+            }
+        }
+        br.close();
+        System.out.println(count);
+    }
+
+    static boolean checkStr(String str){
+
+        boolean[] checkAlpha = new boolean[26]; 
+        int prev = 0;
+        
+        for(int i = 0; i < str.length(); i++){
+            int now = str.charAt(i);
+
+            if(prev != now){ 
+                if(checkAlpha[now-97] == false){
+                    checkAlpha[now-97] = true;
+                    prev = now;
+
+                }else{
+                    return false; 
+                }
+            }else{ 
+                continue;
+            }
+        }
+        return true;
+    }
+}
