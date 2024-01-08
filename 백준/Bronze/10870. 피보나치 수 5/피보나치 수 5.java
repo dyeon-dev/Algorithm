f70d8b2900314146a1cebec2b6c3aa7a38a883cf
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-01-08 20:14:42 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        
+        System.out.println(fibo(N));
+    }
+        static int fibo(int N) {
+            if(N==0) return 0;
+            if(N==1) return 1;
+            return fibo(N-1) + fibo(N-2);
+        }
+    
+}
