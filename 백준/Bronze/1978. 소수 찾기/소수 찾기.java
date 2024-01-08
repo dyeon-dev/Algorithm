ef4c022e167887f2ca072efa83911f6550046bd6
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-01-08 20:00:09 +0900

    [Bronze II] Title: 소수 찾기, Time: 216 ms, Memory: 17812 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -6,13 +6,13 @@ public class Main {
         int N = sc.nextInt();
         int num;
         int count = 0;
-        for(int i=0; i<N; i++){ // N까지 num을 대입 받고 반복한다.
-            num = sc.nextInt(); // num을 받는다.
-            for(int j=2; j<=num; j++){// 2부터 num까지 나눈다.
-                if(j==num) { // 만약 j하고 num이 같으면 자기 자신까지 온 거니까 count++ 한다.
-                    count++; // j가 num하고 같지 않으면 밑으로 내려간다.
+        for(int i=0; i<N; i++){ 
+            num = sc.nextInt(); 
+            for(int j=2; j<=num; j++){
+                if(j==num) {
+                    count++;
                 }
-                if(num%j==0){// 만약 num을 j나눈 나머지가 0이되면 빠져 나온다.
+                if(num%j==0){
                     break;
                 }
             }
