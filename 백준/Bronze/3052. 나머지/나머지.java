0df39fade310a09da9c6ef730763ba584837ba95
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-01-31 14:12:18 +0900

    [Bronze II] Title: 나머지, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,24 +1,14 @@
+import java.util.HashSet;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10]; // 10개의 배열 생성
-        boolean bl; // n번째와 n+1번째랑 비교하기 위한 변수
-        int cnt = 0; // 서로 다른 나머지 개수
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt() % 42;
+        HashSet<Integer> h = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            h.add(sc.nextInt() % 42);
+            // 입력 받은 값의 나머지 값을 add메소드를 통해 HashSet에 저장
         }
-        for(int i=0; i<arr.length; i++) {
-            bl=false; // 비교하기 위한 변수를 false로 초기설정
-            for(int k=i+1; k<arr.length; k++) {
-                if(arr[i]==arr[k]) { // 배열의 n번째와 n+1번째 값을 비교
-                    bl = true; // 값이 같다면 true로 변경
-                    break; // 조건이 성립하면 벗어나기
-                }
-            }
-            if(bl==false) cnt++; // bl의 값이 falsedlf Eo zkdnsxm wmdrk
-            //만약 true면 나머지가 서로 같다는 뜻이니 카운트를 올릴 필요x
-        }
-        System.out.println(cnt);
+        System.out.println(h.size());
     }
 }
