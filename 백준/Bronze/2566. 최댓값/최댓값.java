59fc231c4f5e583636800e45877a90454f3a250b
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-02-08 23:28:59 +0900

    [Bronze III] Title: 최댓값, Time: 240 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[9][9];
+        int max = 0;
+        int n =1, m = 1;
+        for(int i = 0; i<9; i++) {
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0; i<9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(arr[i][j]>max) {
+                    max=arr[i][j];
+                    n=i+1;
+                    m=j+1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(n+" "+m);
+    }
+}
