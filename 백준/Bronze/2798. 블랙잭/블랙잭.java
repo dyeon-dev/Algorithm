fdac312c76495d026c9a5906d6210418c4b4012c
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-02-16 22:05:31 +0900

    [Bronze II] Title: 블랙잭, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,40 +1,26 @@
 import java.util.Scanner;
- 
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		int M = in.nextInt();
- 
-		int[] arr = new int[N];
- 
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
-		}
-		
-		int result = search(arr, N, M);
-		System.out.println(result);
-	}
- 
-	
-	static int search(int[] arr, int N, int M) {
-		int result = 0;
- 
-		for (int i = 0; i < N - 2; i++) {
-			for (int j = i + 1; j < N - 1; j++) {
-				for (int k = j + 1; k < N; k++) {
-					int temp = arr[i] + arr[j] + arr[k];
-					if (M == temp) {	
-						return temp;
-					}
-					if(result < temp && temp < M) {
-						result = temp;
-					}
-				}
-			}
-		}
-		return result;
-	}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int sum = 0;
+        int tmp = 0;
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    sum =arr[i]+arr[j]+arr[k];
+                    if(tmp<sum && sum<=m) tmp=sum;
+                }
+            }
+        }
+        System.out.print(tmp);
+    }
 }
