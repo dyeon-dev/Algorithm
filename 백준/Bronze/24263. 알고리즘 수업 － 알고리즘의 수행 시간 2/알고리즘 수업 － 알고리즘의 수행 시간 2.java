ce59a86e40b958f3e92ec43c05bb8d99df028460
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-03-11 12:13:59 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
