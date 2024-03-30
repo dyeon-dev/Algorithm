debf1fd535c60578c5be008c83c40dcc0d195116
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-03-30 16:49:11 +0900

    [Silver I] Title: 미로 탐색, Time: 160 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,26 +1,28 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N,M;
-    static boolean[][] visited;
+    static boolean visited[][];
     static int[][] A;
 
     // 상하좌우를 탐색하기 위한 배열
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         A = new int[N][M];
         visited = new boolean[N][M]; // 방문 배열 초기화하기
 
-        String str;
         for (int i=0; i<N; i++) {
-            str= sc.next();
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
             for (int j=0; j<M; j++) {
-                A[i][j] = str.charAt(j)-'0';
+                A[i][j] = Integer.parseInt(line.substring(j,j+1));
             }
         }
         BFS();
@@ -28,31 +30,26 @@ public class Main {
         System.out.println(A[N-1][M-1]);
     }
 
-    public static void BFS() {
-        var queue = new LinkedList<int[]>();
+    private static void BFS() {
+        Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[] {0, 0});
-        int[] tmp = new int[2];
 
         visited[0][0] = true; // 첫 방문 지역은 탐색한 상태에서 시작
 
-        int y,x;
-        int ny, nx;
-        
+
         while(!queue.isEmpty()) {
-            tmp = queue.poll();
-            y=tmp[0];
-            x=tmp[1];
+            int[] now = queue.poll();
 
             for (int i=0; i<4; i++) { // 상하좌우 탐색1
-                ny = y + dy[i];
-                nx = x + dx[i];
-                if (nx<0 || ny<0 || ny>=N || nx>=M) continue; // 좌표유효성검사
-                    if (A[ny][nx]==1 && !visited[ny][nx]) {
-                        visited[ny][nx] =true;
-                        A[ny][nx] = A[y][x]+1;
-                        queue.add(new int[] {ny,nx});
+                int x = now[0] + dx[i];
+                int y = now[1] + dy[i];
+                if (x>=0 && y>=0 && x<N && y<M) { // 좌표유효성검사
+                    if (A[x][y]!=0 && !visited[x][y]) {
+                        visited[x][y] =true;
+                        A[x][y] = A[now[0]][now[1]]+1;
+                        queue.add(new int[] {x,y});
                     }
-                
+                }
             }
         }
     }
