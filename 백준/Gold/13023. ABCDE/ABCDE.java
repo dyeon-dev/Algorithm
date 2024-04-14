168e2ec366922aef8538faa3ea175c04e193f7bc
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-04-14 21:37:31 +0900

    [Gold V] Title: ABCDE, Time: 516 ms, Memory: 24788 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,46 +1,54 @@
 import java.util.*;
-import java.io.*;
 public class Main {
-    static ArrayList<Integer>[] A;
-    static boolean arrive; // 도착확인변수
+
+    static int n,m;
     static boolean visited[];
+    static ArrayList<Integer>[] A;
+
+    static boolean arrive;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        visited = new boolean[N]; // 방문 기록 저장 배열
-        A = new ArrayList[N];
 
-        // A 인접 리스트의 각 ArrayList 초기화하기
-        for(int i=0; i<N; i++) {
-            A[i] = new ArrayList<Integer>();
+        n = sc.nextInt(); // 노드 수
+        m = sc.nextInt(); // 간선 수
+        A= new ArrayList[n];
+        // 방문 배열 초기화
+        visited = new boolean[n];
+        arrive=false;
+
+        for (int i = 0; i <n; i++) {
+            A[i]=new ArrayList<>();
         }
-        // 인접 리스트에 그래프 데이터 저장하기
-        for(int i=0; i<M; i++) {
+        for (int i = 0; i < m; i++) {
             int s = sc.nextInt();
             int e = sc.nextInt();
             A[s].add(e);
             A[e].add(s);
         }
-        for (int i=0; i<N; i++) {
-            DFS(i, 1);
-            if(arrive) break; // depth가 5에 도달한 적이 있다면 종료
+        for (int i = 0; i < n; i++) {
+            dfs(i, 1);
+            if (arrive) break;
         }
-        if (arrive) System.out.println("1");
-        else System.out.println("0");
+
+
+        if (arrive) System.out.println(1);
+        else System.out.println(0);
     }
-    public static void DFS(int v, int depth) {
-        //System.out.println(v + " " + depth);
-        if (depth==5 || arrive) { // depth가 5가 되면 프로그램 종료
+
+    private static void dfs(int index, int depth) {
+        if (depth==5) {
             arrive=true;
             return;
         }
-        visited[v] = true;
-        for (int i: A[v]) {
-            if (!visited[i]) DFS(i, depth+1); // 재귀호출이 될 때마다 depth 1씩 증가
-        }
-        visited[v] = false;
-    }
+        visited[index] = true;
 
+        for (int i = 0; i < A[index].size(); i++) {
+            int node = A[index].get(i);
+            if (!visited[node]) {
+                dfs(node, depth+1);
+            }
+        }
+        visited[index]=false;
+    }
 }
