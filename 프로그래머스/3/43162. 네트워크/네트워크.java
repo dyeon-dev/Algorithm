d383f06bc971ab1cc7541ddc314a42fda7eec9b2
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-04-18 21:24:35 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,45 @@
+import java.util.*;
+
 class Solution {
     int answer = 0;
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[computers.length];
-        for(int i=0; i<computers.length; i++) {
+        int answer=0;
+        boolean[] visited=new boolean[computers.length];
+        for(int i =0; i<computers.length; i++) {
             visited[i]=false;
         }
-        
-        for(int i=0; i<computers.length; i++) {
-            if(visited[i]==false) {
+        for(int i =0; i<computers.length; i++) {
+            if(!visited[i]) {
+                bfs(i, computers, visited);
                 answer++;
-                dfs(i, computers, visited);
             }
+            
         }
         return answer;
     }
     public void dfs(int n, int[][] computers, boolean[] visited) {
         visited[n]=true;
         for(int i=0; i<computers.length; i++) {
-            if(!visited[i] && computers[n][i]==1) {
+            if(visited[i]==false && computers[n][i]==1) {
                 dfs(i, computers, visited);
-            } 
+            }
         }
     }
+    
+    public void bfs(int n, int[][] computers, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        visited[n]=true;
+        
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            for(int i=0; i<computers.length; i++) {
+                if(!visited[i] && computers[now][i]==1) {
+                visited[i]=true;
+                q.add(i);
+                }   
+            }
+        }
+    }
+    
 }
