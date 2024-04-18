2ac8038d2f6b59502d13eed740e8f9779e1427c6
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-04-18 22:18:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.52 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,31 +2,31 @@ import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int answer = 0;
-        
         boolean[][] visited = new boolean[maps.length][maps[0].length];
+        
         
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[] {0,0});
-        visited[0][0] = true;
+        visited[0][0]=true;
         
         while(!q.isEmpty()) {
             int[] now = q.poll();
-            int[] dx = {-1,1,0,0};
-            int[] dy = {0,0,-1,1};
+            int[] dx={-1,1,0,0};
+            int[] dy={0,0,-1,1};
             
             for(int i=0; i<4; i++) {
-                int nx = now[0]+dx[i];
-                int ny = now[1]+dy[i];
-                if(nx>=0 && ny>=0 && nx<=maps.length-1 && ny<=maps[0].length-1 
-                   && maps[nx][ny]==1 && !visited[nx][ny]) {
+                int nx=now[0]+dx[i];
+                int ny=now[1]+dy[i];
+                if(nx>=0 && ny>=0 && nx<maps.length && ny<maps[0].length
+                  && !visited[nx][ny] && maps[nx][ny]==1){
                     visited[nx][ny]=true;
-                    maps[nx][ny] = maps[now[0]][now[1]]+1;
-                    q.add(new int[] {nx, ny});
+                    maps[nx][ny]=maps[now[0]][now[1]]+1;
+                    q.add(new int[] {nx,ny});
                 }
             }
         }
-        answer=maps[maps.length-1][maps[0].length-1];
-        if(!visited[maps.length-1][maps[0].length-1]) return -1;
+        answer = maps[maps.length-1][maps[0].length-1];
+        if(visited[maps.length-1][maps[0].length-1]==false) return -1;
         return answer;
     }
 }
