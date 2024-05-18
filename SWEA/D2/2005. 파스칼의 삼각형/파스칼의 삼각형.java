7ca82ff4cc4aebcbc520c93bd1b18d0bda8a042a
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-05-18 22:06:26 +0900

    [D2] Title: 파스칼의 삼각형, Time: 189 ms, Memory: 26,160 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         int T = sc.nextInt();
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int N = sc.nextInt();
-            int[][] map = new int[N][N];
-            for (int i = 0; i < N; i++) {
+        for(int t = 1; t <= T; t++) {
+            int n = sc.nextInt();
+            int[][] ans = new int[n][n];
+            for (int i = 0; i < n; i++) {
                 for (int j = 0; j <= i; j++) {
-                    if (i == j) {
-                        map[i][j] = 1;
-                    } else if (j == 0) {
-                        map[i][j] = 1;
-                    } else {
-                        map[i][j] = map[i - 1][j - 1] + map[i - 1][j];
-                    }
+                    if (i==j) ans[i][j] = 1;
+                    else if (j==0) ans[i][j] = 1;
+                    else ans[i][j] = ans[i-1][j-1]+ans[i-1][j];
                 }
             }
-            System.out.println("#" + test_case);
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j <= i; j++) 
-                    System.out.print(map[i][j] + " ");          
+            System.out.println("#" + t + " ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(ans[i][j]+" ");
+                }
                 System.out.println();
             }
         }
