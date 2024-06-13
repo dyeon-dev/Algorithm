1e4d73a4710daf01384a6c561953f6949cc2b6e1
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-06-13 14:04:55 +0900

    [Bronze II] Title: 나머지, Time: 200 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,14 +1,24 @@
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashSet<Integer> h = new HashSet<>();
-
-        for (int i = 0; i < 10; i++) {
-            h.add(sc.nextInt() % 42);
-            // 입력 받은 값의 나머지 값을 add메소드를 통해 HashSet에 저장
+        int[] rest = new int[10];
+        boolean b;
+        int cnt =0;
+        for (int i = 0; i < rest.length; i++) {
+            rest[i] = sc.nextInt()%42;
         }
-        System.out.println(h.size());
+        for (int i = 0; i < rest.length; i++) {
+            b = false; // 기본값 초기화
+            for (int j = i+1; j < rest.length; j++) {
+                if(rest[i]==rest[j]) {
+                    b=true; // 나머지가 같을 때 반복문 종료
+                    break;
+                }
+            }
+            if (!b) cnt++;
+        }
+        System.out.println(cnt);
     }
 }
