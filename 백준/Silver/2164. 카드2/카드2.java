b5d3ac38b2a962563d9dd732e3edb0ecf2ff3d4c
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-06-19 14:24:50 +0900

    [Silver IV] Title: 카드2, Time: 272 ms, Memory: 49064 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,19 +1,17 @@
 import java.util.*;
-import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<>();
-        int N = in.nextInt();
-
-        for(int i = 1; i <= N; i++) {
-            queue.add(i);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            q.add(i);
         }
-        while(queue.size()>1) { // 카드가 1장 남을 때까지
-            queue.poll(); // 맨 위의 카드를 버림
-            queue.add(queue.poll()); // 맨 위의 카드를 가장 아래 카드 밑으로 이동
+        while(q.size()!=1) {
+            q.poll();
+            q.add(q.poll());
         }
-        System.out.println(queue.poll());
+        System.out.print(q.poll());
     }
 }
