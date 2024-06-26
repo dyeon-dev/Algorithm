d741464887d2e2ce68e2ecef328451244b56faeb
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-06-26 15:06:53 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3580 ms, Memory: 361100 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,82 @@
+import java.util.*;
+class Edge implements Comparable<Edge> {
+    private int distance;
+    private int nodeA;
+    private int nodeB;
+
+    public Edge (int distance, int nodeA, int nodeB) {
+        this.distance=distance;
+        this.nodeA=nodeA;
+        this.nodeB=nodeB;
+    }
+    public int getDistance() {
+        return this.distance;
+    }
+    public int getNodeA() {
+        return this.nodeA;
+    }
+    public int getNodeB() {
+        return this.nodeB;
+    }
+    // 거리(비용)가 짧은 것이 높은 우선순위를 가지도록 설정
+    @Override
+    public int compareTo(Edge o) {
+        return this.distance-o.distance;
+    }
+}
+public class Main{
+    static int N,M;
+    static int[] parent = new int[1000001]; // 부모 테이블 초기화하기
+    // 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
+    static ArrayList<Edge> edges = new ArrayList<>();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt(); // 집의 개수
+        M = sc.nextInt(); // 길의 개수
+
+        // 부모 테이블 상에서, 부모를 자기 자신으로 초기화
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        // Union 연산을 각각 수행
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt(); // 시작점
+            int b = sc.nextInt(); // 종료점
+            int cost = sc.nextInt(); // 유지비
+            edges.add(new Edge(cost, a, b));
+        }
+        // 간선을 비용순으로 정렬
+        Collections.sort(edges);
+
+        int ans=0;
+        int max=0;
+        // 간선을 하나씩 확인하며
+        for (int i = 0; i < edges.size(); i++) {
+            int cost = edges.get(i).getDistance();
+            int a = edges.get(i).getNodeA();
+            int b = edges.get(i).getNodeB();
+            // 사이클이 발생하지 않는 경우에만 집합에 포함
+            if (findParent(a)!=findParent(b)) {
+                ans+=cost; // 모든 간선들의 가중치 합
+                unionParent(a,b);
+                max=cost; // 현재까지 선택된 간선 중 가장 큰 가중치 
+            }
+        }
+        System.out.println(ans-max); // 가장 큰 가중치의 간선을 제거한 후의 비용, 즉 두 마을로 나눴을 때 최솟값의 유지비 
+    }
+    // 두 원소가 속한 집합 합치기
+    private static void unionParent(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+        if (a<b) parent[b]=a;
+        else parent[a]=b;
+    }
+
+    // 특정 원소가 속한 집합 찾기
+    private static int findParent(int x) {
+        // 루트노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+        if (x==parent[x]) return x;
+        return parent[x]=findParent(parent[x]);
+    }
+}
