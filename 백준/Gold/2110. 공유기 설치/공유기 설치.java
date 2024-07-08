cfb0a2d785670cbac0d24e4dc251278e973e8242
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-07-08 11:44:03 +0900

    [Gold IV] Title: 공유기 설치, Time: 1136 ms, Memory: 176480 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,38 +1,38 @@
-    import java.util.Arrays;
-    import java.util.Scanner;
+import java.util.*;
 
-    class Main {
+public class Main {
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int N = sc.nextInt(); //도현이의 집
-            int C = sc.nextInt(); // 공유기 개수
-            int[] house = new int[N];
-            int answer = 0;
-            for (int i = 0; i < N; i++) {
-                house[i] = sc.nextInt();
-            }
-            Arrays.sort(house);
-            int left = 1; //최소 길이
-            int right = house[N - 1] - house[0];// 최대 길이
-            while (left <= right) {
-                int mid = (left + right) / 2; //공유기 거리 기준
-                int prevHouse = house[0]; //첫 위치에 설치
-                int count = 1; //공유기 설치 개수
-                for (int i = 0; i < N; i++) {
-                    int distance = house[i] - prevHouse;
-                    if (distance >= mid) { //거리차가 기준보다 이상되야 설치 가능
-                        count++;
-                        prevHouse = house[i];
-                    }
-                }
-                if (count >= C) { //공유기 설치가 더 많이 됬으니 간격을 넓혀서 줄여야함
-                    left = mid + 1;
-                    answer = mid;
-                } else {
-                    right = mid - 1;
-                }
-            }
-            System.out.println(answer);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
+        Arrays.sort(arr);
+        int pl=1; // 최소길이: 맨 앞집의 위치
+        int pr=arr[n-1]-arr[0]; // 최대길이: 맨 끝집의 위치-맨 앞집의 위치
+
+        int ans=0;
+        while(pl<=pr) {
+            int pc=(pl+pr)/2;
+            int first = arr[0]; // 첫번째 집은 무조건 설치
+            int cnt=1; // 설치 개수
+            for (int i = 0; i < n; i++) {
+                int d = arr[i]-first; // 현재위치에서 거리 차이
+                if (d>=pc) { // 거리 차이가 기준보다 크거나 같을 때 설치 가능
+                    cnt++;
+                    first=arr[i];
+                }
+            }
+            if (cnt>=c) { // 설치가 더 많이 됐거나 갯수만큼 설치됐으면 범위를 넓혀서 탐색
+                pl=pc+1;
+                ans=pc; // 최적의 결과 저장
+            } else { // 설치가 덜 됐으면 범위를 좁혀서 탐색
+                pr=pc-1;
+            }
+        }
+        System.out.println(ans);
     }
+}
