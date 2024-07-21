bd5c07a22353bbd49acb42c5e73de9a019351624
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-07-21 20:17:44 +0900

    [level 3] Title: 주사위 고르기, Time: 966.84 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+class Solution {
+    static int n;
+    static int[][] dices;
+    static List<Integer> com = new ArrayList<>();
+    static List<Integer> arrA;
+    static List<Integer> arrB;
+    static int max = Integer.MIN_VALUE;
+    static int[] answer;
+
+    public int[] solution(int[][] dice) {
+        n = dice.length;
+        dices = dice;
+        answer = new int[n / 2];  // answer 배열 초기화
+        combinationA(0, 0);
+        return answer;
+    }
+
+    // 1. A가 가져갈 수 있는 주사위의 조합 구하기 (재귀 사용)
+    private void combinationA(int d, int s) {
+        if (d == n / 2) {
+            // 2. 조합을 완성하면 가질 수 있는 눈금의 합을 계산
+            int win = calculateCom();
+
+            if (max < win) {
+                max = win;
+                for (int i = 0; i < com.size(); i++) {
+                    answer[i] = com.get(i) + 1;
+                }
+            }
+            return;
+        }
+        for (int i = s; i < n; i++) {  // 조합 구하는 범위 수정
+            com.add(i);
+            combinationA(d + 1, i + 1);
+            com.remove(com.size() - 1);
+        }
+    }
+
+    private int calculateCom() {
+        int[][] diceA = new int[n / 2][6];
+        int[][] diceB = new int[n / 2][6];
+        arrA = new ArrayList<>();
+        arrB = new ArrayList<>();
+
+        int a = 0, b = 0;
+        for (int i = 0; i < n; i++) {
+            // A주사위 조합
+            if (com.contains(i)) {
+                diceA[a] = dices[i];
+                a++;
+            }
+            // B주사위 조합
+            else {
+                diceB[b] = dices[i];
+                b++;
+            }
+        }
+
+        // 2. 각 주사위 모든 눈금의 합 저장
+        makeArr(0, diceA, 0, arrA);
+        makeArr(0, diceB, 0, arrB);
+
+        // 3. 정렬 (이분탐색 예정)
+        Collections.sort(arrA);
+        Collections.sort(arrB);
+
+        int cnt = 0;
+
+        for (int i = 0; i < arrA.size(); i++) {
+            // 4. 이분탐색으로 arrB에 arrA보다 작은 숫자가 몇개 있는지 구하여 그 값을 모두 더하면 A가 승리하는 경우이다.
+            int num = arrA.get(i);
+
+            int left = 0, right = arrB.size() - 1;
+            int index = Integer.MIN_VALUE;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (arrB.get(mid) < num) {
+                    left = mid + 1;
+                    index = Math.max(index, mid); // arrA 합계 num보다 작은 arrB의 최대 인덱스를 찾아 저장
+                } else {
+                    right = mid - 1;
+                }
+            }
+            if (index != Integer.MIN_VALUE) {
+                // arrB에서 작은 값의 수를 모두 더함
+                cnt += index + 1; // index+1은 arrB에서 num보다 작은 모든 값의 수를 의미
+            }
+        }
+        return cnt;
+    }
+
+    private void makeArr(int d, int[][] dice, int sum, List<Integer> arr) {
+        if (d == n / 2) {
+            arr.add(sum);
+            return;
+        }
+        for (int i = 0; i < 6; i++) {
+            int newSum = sum + dice[d][i];
+            makeArr(d + 1, dice, newSum, arr);
+        }
+    }
+}
