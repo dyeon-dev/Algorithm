36cea944e2778efc7ac23f0e4bf354c55488be01
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2024-08-22 13:43:52 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,28 @@
 import java.util.*;
 
 class Solution {
-    int answer = 0;
     public int solution(int n, int[][] computers) {
-        int answer=0;
-        boolean[] visited=new boolean[computers.length];
-        for(int i =0; i<computers.length; i++) {
-            visited[i]=false;
-        }
-        for(int i =0; i<computers.length; i++) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for(int i=0; i<computers.length; i++) {
             if(!visited[i]) {
-                bfs(i, computers, visited);
+                dfs(i, computers, visited);
                 answer++;
             }
-            
         }
-        return answer;
+        return answer; 
     }
-    public void dfs(int n, int[][] computers, boolean[] visited) {
+    
+    static void dfs(int n, int[][] computers, boolean[] visited) {
         visited[n]=true;
         for(int i=0; i<computers.length; i++) {
-            if(visited[i]==false && computers[n][i]==1) {
-                dfs(i, computers, visited);
+            if(!visited[i] && computers[n][i]==1) {
+                bfs(i, computers, visited);
             }
         }
     }
     
-    public void bfs(int n, int[][] computers, boolean[] visited) {
+    static void bfs(int n, int[][] computers, boolean[] visited) {
         Queue<Integer> q = new LinkedList<>();
         q.add(n);
         visited[n]=true;
@@ -35,11 +31,10 @@ class Solution {
             int now = q.poll();
             for(int i=0; i<computers.length; i++) {
                 if(!visited[i] && computers[now][i]==1) {
-                visited[i]=true;
-                q.add(i);
-                }   
+                    visited[i]=true;
+                    q.add(i);
+                }
             }
         }
     }
-    
 }
