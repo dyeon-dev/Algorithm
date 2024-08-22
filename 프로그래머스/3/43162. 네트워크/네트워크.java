b25c8af8939f2c29d8c38aed0c2e51cf846d690d
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2024-08-22 22:46:57 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,40 +1,43 @@
 import java.util.*;
 
 class Solution {
+    static int[] parent;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
-        for(int i=0; i<computers.length; i++) {
-            if(!visited[i]) {
-                dfs(i, computers, visited);
-                answer++;
+        parent = new int[n];        
+        // 대표 노드를 자기 자신으로 초기화
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        // 모든 간선(컴퓨터 연결)을 순회하면서 유니온 파인드 수행
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(computers[i][j] == 1) { 
+                    union(i, j);
+                }
             }
         }
+        
+        // 각 노드의 최종 대표를 확인하여 네트워크의 수를 카운트
+        for(int i = 0; i < n; i++) {
+            if(parent[i] == i) answer++;
+        }
+        
         return answer; 
     }
     
-    static void dfs(int n, int[][] computers, boolean[] visited) {
-        visited[n]=true;
-        for(int i=0; i<computers.length; i++) {
-            if(!visited[i] && computers[n][i]==1) {
-                bfs(i, computers, visited);
-            }
-        }
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a != b) parent[b] = a;
     }
     
-    static void bfs(int n, int[][] computers, boolean[] visited) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(n);
-        visited[n]=true;
-        
-        while(!q.isEmpty()) {
-            int now = q.poll();
-            for(int i=0; i<computers.length; i++) {
-                if(!visited[i] && computers[now][i]==1) {
-                    visited[i]=true;
-                    q.add(i);
-                }
-            }
+    static int find(int a) {
+        if(parent[a] == a) return a;
+        else {
+            return parent[a] = find(parent[a]); // 재귀 함수 형태로 구현 -> 경로 압축 
         }
     }
 }
