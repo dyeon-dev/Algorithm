c553dad6e02a1f71b8eff34f40860ec9ae9dd18c
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2024-08-22 22:23:48 +0900

    [Gold V] Title: 집합의 표현, Time: 1640 ms, Memory: 267236 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,45 +1,53 @@
 import java.util.*;
-public class Main{
-    static int N,M;
-    static int[] parent = new int[1000001]; // 부모 테이블 초기화하기
-    public static void main(String args[]) {
+
+public class Main {
+    static int n,m, kCnt, vCnt, kTotal, vTotal;
+    static boolean[][] visited;
+    static int[] parent;
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt(); // 연산 개수
-
-        // 부모 테이블 상에서, 부모를 자기 자신으로 초기화
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        parent = new int[n+1]; // 대표 노드 저장 배열
+        for (int i = 1; i <= n; i++) {
+            parent[i]=i; // 대표 노드 자기 자신으로 초기화
         }
-
-        // Union 연산을 각각 수행
-        for (int i = 0; i < M; i++) {
-            int num = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            int ques = sc.nextInt();
             int a = sc.nextInt();
             int b = sc.nextInt();
-            if (num==0) unionParent(a, b);
-            else if (num==1) {
-                // a와 b가 같은 집한에 포함되어 있으면 "YES" 없으면 "NO
-                if(findParent(a)==findParent(b)) {
+            if(ques==0) { // union
+                union(a,b);
+            } else { // find
+                // 같은 집합 원소인지 확인하기
+                if(checkSame(a, b)) {
                     System.out.println("YES");
-                } else {
-                 System.out.println("NO");
-                }
+                } else System.out.println("NO");
             }
         }
     }
     // 두 원소가 속한 집합 합치기
-    private static void unionParent(int a, int b) {
-        a = findParent(a);
-        b = findParent(b);
-        if (a<b) parent[b]=a;
-        else parent[a]=b;
+    static void union(int a, int b) {
+        // 대표 노드 찾기
+        a = find(a);
+        b = find(b);
+
+        // 두 원소의 대표 노드끼리 연결하기
+        if(a!=b) { // 대표 노드가 다르면
+            parent[b]=a; // 두개 연결
+        }
+    }
+    // 특정 원소가 속한 집합 찾기
+    static int find(int a) {
+        if(a==parent[a]) return a;
+        else return parent[a]=find(parent[a]); // 재귀 함수의 형태로 구현->경로 압축 부분
     }
 
-    // 특정 원소가 속한 집합 찾기
-    private static int findParent(int x) {
-        // 루트노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
-        if (x==parent[x]) return x;
-        return parent[x]=findParent(parent[x]);
+    static boolean checkSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a==b) return true;
+        else return false;
     }
 }
