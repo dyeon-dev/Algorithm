ccbb77288b424e5e6190fdc25f0d9ceccf71d53a
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-01-22 02:04:58 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 92 ms, Memory: 9484 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
-const inputData = fs.readFileSync('/dev/stdin').toString();
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const num = Number(inputData);
-let result = '';
+let a = Number(input[0]);
 
-// 누적합을 매 반복마다 출력
-for(let i = 1; i <= num; i++){
-	result += '*';
-  	console.log(result);
+let result = "";
+for(let i=0; i<a; i++) {
+    for(let j=0; j<=i; j++) {
+        result += "*";
+    }
+    result += "\n"
 }
+console.log(result);
