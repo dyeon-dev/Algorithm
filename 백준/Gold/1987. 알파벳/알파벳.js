85763826876daa50fb094804c37f8735d2dc889f
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-02-05 10:58:42 +0900

    [Gold IV] Title: 알파벳, Time: 7992 ms, Memory: 14740 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -0,0 +1,34 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let [r, c] = input[0].split(" ").map(Number); // 세로 r, 가로 c
+let arr = [];
+for (let i = 1; i <= r; i++) {
+  arr.push(input[i]);
+}
+
+let visited = new Set(); // 방문한 적 있는 알파벳 집합
+let max = 0; // 최대 깊이
+
+let dx = [0, 0, 1, -1];
+let dy = [1, -1, 0, 0];
+
+function dfs(x, y, depth) {
+  max = Math.max(max, depth);
+
+  for (let i = 0; i < 4; i++) {
+    let nx = x + dx[i];
+    let ny = y + dy[i];
+    if (nx >= 0 && ny >= 0 && nx < r && ny < c && !visited.has(arr[nx][ny])) {
+      visited.add(arr[nx][ny]);
+      dfs(nx, ny, depth + 1);
+      visited.delete(arr[nx][ny]);
+    }
+  }
+}
+
+// 좌측 상단부터 시작
+visited.add(arr[0][0]);
+dfs(0, 0, 1);
+
+console.log(max);
