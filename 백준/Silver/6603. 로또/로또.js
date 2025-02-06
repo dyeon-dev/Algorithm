35bcb87c5e76368bcc05f0631f506a93e9179434
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-02-06 14:14:30 +0900

    [Silver II] Title: 로또, Time: 156 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.js
+++ 백준/Silver/6603. 로또/로또.js
@@ -1,44 +1,32 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+for (let t = 0; t < input.length; t++) {
+  let arr = input[t].split(" ").map(Number);
+  if (arr[0] == 0) break;
+  let k = arr[0];
+  let s = arr.splice(1);
+  let visited = new Array(k).fill(false);
+  let result = [];
+  let answer = "";
 
-// S={1,2,3,4,5,6,7,8} K=8일 경우 모든 조합의 수 Combination(8,6)=28
+  function dfs(depth, start) {
+    if (depth == 6) {
+      for (x of result) {
+        answer += s[x] + " ";
+      }
+      answer += "\n";
+    }
 
-for (let i = 0; i < input.length; i++) {
-    let data = input[i].split(' ').map(Number);
-    if (data[0]==0) break;
-    else {
-        n = data[0];
-        arr = data.splice(1);
-        visited = new Array(n).fill(false);
-        selected=[];
-        ans='';
-        
-        dfs(0, 0, arr);
-        
-        console.log(ans);
+    for (let i = start; i < k; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        result.push(i);
+        dfs(depth + 1, i + 1);
+        visited[i] = false;
+        result.pop();
+      }
     }
-}
-
-function dfs(depth, start, arr) {
-    if (depth==6) {
-        result=[];
-        for(i of selected) {
-            result.push(arr[i]);
-        }
-        for(x of result) {
-            ans += x + ' ';
-        }
-        ans += '\n';
-        return;
-    }
-    
-    for(let i = start; i < n; i++) {
-        if(!visited[i]) {
-            visited[i]=true;
-            selected.push(i);
-            dfs(depth+1, i+1, arr);
-            selected.pop();
-            visited[i]=false;
-        }
-    }
+  }
+  dfs(0, 0);
+  console.log(answer);
 }
