cb5a6940c93e954c089e03d730453771e0a1baa4
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-02-09 12:47:26 +0900

    [Gold IV] Title: 이분 그래프, Time: 1560 ms, Memory: 152428 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.js
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.js
@@ -4,44 +4,58 @@ let k = Number(input[0]);
 let index = 1;
 let result = [];
 
-function dfs(cur, graph, color) {
-  for (let next of graph[cur]) {
-    if (color[next] === -1) {
-      color[next] = 1 - color[cur]; // 현재 노드와 다른 색 부여
-      if (!dfs(next, graph, color)) return false;
-    } else if (color[next] === color[cur]) {
-      return false; // 같은 색이 나오면 이분 그래프가 아님
+function bfs(cur, graph, color) {
+  let queue = [cur];
+  color[cur] = 1;
+
+  while (queue.length != 0) {
+    let current = queue.shift();
+
+    for (let i of graph[current]) {
+      if (color[i] == -1) {
+        color[i] = 1 - color[current];
+        queue.push(i);
+      } else if (color[i] == color[current]) return false;
     }
   }
   return true;
 }
 
+// function dfs(cur, graph, color) {
+//   for (let i of graph[cur]) {
+//     if (color[i] == -1) {
+//       color[i] = 1 - color[cur];
+//       if (!dfs(i, graph, color)) return false;
+//     } else if (color[i] == color[cur]) return false;
+//   }
+//   return true;
+// }
+
 for (let t = 0; t < k; t++) {
   let [V, E] = input[index].split(" ").map(Number);
   index++;
+  let graph = [];
+  let color = new Array(V + 1).fill(-1);
+  let isTrue = true;
 
-  let graph = Array.from({ length: V + 1 }, () => []);
+  for (let i = 0; i <= V; i++) graph.push([]);
   for (let i = 0; i < E; i++) {
     let [u, v] = input[index].split(" ").map(Number);
     index++;
-    graph[u].push(v);
     graph[v].push(u);
+    graph[u].push(v);
   }
 
-  let color = new Array(V + 1).fill(-1);
-  let isBipartite = true;
-
   for (let i = 1; i <= V; i++) {
-    if (color[i] === -1) {
-      ///color[i] = 0;
-      if (!dfs(i, graph, color)) {
-        isBipartite = false;
+    if (color[i] == -1) {
+      let res = bfs(i, graph, color);
+      if (!res) {
+        isTrue = false;
         break;
       }
     }
   }
-
-  result.push(isBipartite ? "YES" : "NO");
+  result.push(isTrue ? "YES" : "NO");
 }
 
 console.log(result.join("\n"));
