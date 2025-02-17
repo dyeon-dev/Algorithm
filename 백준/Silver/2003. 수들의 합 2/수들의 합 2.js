ba59a0242b5a4280f5c43e2290e34ae85f231315
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-02-17 23:59:06 +0900

    [Silver IV] Title: 수들의 합 2, Time: 96 ms, Memory: 9780 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -0,0 +1,22 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let [n, m] = input[0].split(" ").map(Number);
+let arr = input[1].split(" ").map(Number);
+let cnt = 0;
+
+let sum = 0;
+let end = 0; 
+
+for(let start=0; start<n; start++) {
+  while(sum<m && end<n) {
+    sum+=arr[end];
+    end++;
+  }
+
+  if(sum==m) {
+    cnt++;
+  }
+  sum-=arr[start];
+}
+console.log(cnt);
