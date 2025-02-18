0237cc731ac6aee0a19922c6f69225758af65e5c
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-02-18 15:13:51 +0900

    [Gold I] Title: K번째 수, Time: 688 ms, Memory: 53012 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.js
+++ 백준/Gold/1300. K번째 수/K번째 수.js
@@ -1,27 +1,26 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-// 정렬된 값: [1,2,2,3,3,4,4,4,6,6,8,8,9,12,12,16] 여기서 k번째 수
-// "현재 mid보다 작거나 같은 데이터의 수가 k개 이상이 되는 조건"을 만족하는 mid 중에서 가장 작은 값을 구하면 된다
+
 let n = Number(input[0]);
 let k = Number(input[1]);
+let res = 0;
 
-let start = 1;
-let end = n*n; // 배열에 존재할 수 있는 가장 큰 값 (10**10)
+// 정렬 이후에 k번째 수를 구하는 것이 목표이다. n=4, k=14일때, 답은 12다.
+// 초기 설정: start=1, end=16을 두고, mid를 기준으로 행마다 mid보다 작거나 같은 원소의 개수를 구한다.
+// mid가 12일 때 => 작거나 같은 원소의 개수: 15>=14 가장 작은 mid 값으로 조건을 만족하는 값이다
 
-let result = 0;
-while(start <= end) {
-  let mid = parseInt((start+end)/2);
-  let total=0;
-  for(let i=1; i<=n; i++) {
-    // mid보다 작거나 같은 원소의 개수
-    total+=Math.min(parseInt(mid/i), n); // 4+4+2+2=12
+let start = 0;
+let end = n * n;
+
+while (start <= end) {
+  let mid = parseInt((start + end) / 2);
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += parseInt(Math.min(n, mid / i));
   }
-  if(total>=k) { // mid보다 작거나 같은 데이터의 개수가 k 이상이라면
-    result=mid; // result에 기록
-    end=mid-1;
-  } else {
-    start=mid+1;
-  }
+  if (total >= k) {
+    end = mid - 1;
+    res = mid;
+  } else start = mid + 1;
 }
-
-console.log(result);
+console.log(res);
