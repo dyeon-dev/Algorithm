be46bf28ae45a3e4437f9132bfa0374af1ce6980
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-03-26 11:33:36 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 3199.74 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,17 @@
+function solution(bridge_length, weight, truck_weights) {
+    let time=0;
+    let q = new Array(bridge_length).fill(0);
+    while(q.length) {
+        q.shift();
+        if(truck_weights.length) {
+            let sum = q.reduce((a, b)=>a+b, 0);
+            if(sum+truck_weights[0]<=weight) {
+                q.push(truck_weights.shift());
+            } else {
+                q.push(0);
+            }
+        }
+        time++;
+    }
+    return time
+}
