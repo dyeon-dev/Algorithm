33f252c0bad8c96ae10d266b039ae382554c9c46
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-04-15 02:26:24 +0900

    [level 1] Title: 달리기 경주, Time: 115.99 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -2,17 +2,19 @@ function solution(players, callings) {
     const playerMap = {};
     
     for(let i=0; i<players.length; i++) {
-        playerMap[players[i]] = i;
+        playerMap[players[i]]=i;
     }
     
     for(let i=0; i<callings.length; i++) {
-        const idx = playerMap[callings[i]];
-        const temp = players[idx-1];
+        let idx = playerMap[callings[i]];
+        let temp = players[idx-1];
+        
         players[idx-1]=callings[i];
         players[idx]=temp;
-
-        playerMap[callings[i]] = idx-1;
+        
+        playerMap[callings[i]]=idx-1;
         playerMap[temp]=idx;
+        
     }
     
     return players;
