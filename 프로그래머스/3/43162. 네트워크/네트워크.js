3d930cb273506ba4f7741e024631605846045432
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-05-02 18:01:39 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,36 +1,36 @@
 function solution(n, computers) {
-    let ans=0;
-    let visited = new Array(n).fill(false);
-    
-    function dfs(x) {
-        visited[x]=true;
-        for(let i=0; i<n; i++) {
-            if(computers[x][i]==1 && !visited[i]) {
-                dfs(i);
-            }
-        }
+  let parent = [];
+  for(let i=0; i<=n; i++) {
+      parent[i]=i;
+  }
+
+  // Find 함수: 노드 x의 루트 노드를 찾는다
+  function find(x) {
+    if (parent[x] === x) return x;
+    return find(parent[x]);
+  }
+
+  // Union 함수: 노드 x와 노드 y를 연결한다
+  function union(x, y) {
+    const rootX = find(x);
+    const rootY = find(y);
+    if (rootX !== rootY) parent[rootY] = rootX; // 루트가 다르면 하나의 루트로 합친다
+  }
+
+  // 모든 컴퓨터를 순회하며 연결된 컴퓨터들을 합친다
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (computers[i][j] === 1) {
+        union(i, j);
+      }
     }
-    
-    function bfs(x) {
-        let q = [];
-        q.push(x);
-        visited[x]=true;
-        while(q.length!=0) {
-            let now = q.shift();
-            for(let i=0; i<n; i++) {
-                if(computers[now][i]==1 && !visited[i]) {
-                    visited[i]=true;
-                    q.push(i);
-                }
-            }
-            
-        }
-    }
-    for(let i=0; i<n; i++) {
-        if(!visited[i]) {
-            bfs(i);
-            ans++;
-        }
-    }
-    return ans;
+  }
+
+  // 각 노드가 자기 자신을 가리키는 경우만 카운트 (즉, 루트 노드인 경우)
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    if (find(i) === i) answer++;
+  }
+
+  return answer;
 }
