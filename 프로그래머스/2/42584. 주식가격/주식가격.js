054a69ba1f291b4773505edf3a4c2584a91f5f2e
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-05-13 18:52:08 +0900

    [level 2] Title: 주식가격, Time: 6.73 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,14 +1,12 @@
 function solution(prices) {
     const answer = [];
-    for (let i = 0; i < prices.length; i++) {
-        let stack = 0;
-        for (let j = i + 1; j < prices.length; j++) {
-            stack++;
-            if (prices[i] > prices[j]) {
-                break;
-            }
+    for(let i=0; i<prices.length; i++) {
+        let cnt=0;
+        for(let j=i+1; j<prices.length; j++) {
+            cnt++;
+            if(prices[i]>prices[j]) break;
         }
-        answer.push(stack);
+        answer.push(cnt);
     }
     return answer;
 }
