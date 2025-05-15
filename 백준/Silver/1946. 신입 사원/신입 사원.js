0c3225b8c7d6eb4871c2da2ec69a75e35e249d81
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-05-15 14:53:34 +0900

    [Silver I] Title: 신입 사원, Time: 3196 ms, Memory: 412848 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.js
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -1,25 +1,26 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let t = Number(input[0]);
-let line = 1;
+let t = input[0];
+let idx = 1;
 for (let i = 0; i < t; i++) {
-  let n = Number(input[line]);
+  let n = Number(input[idx]);
   let arr = [];
-  for (let j = line + 1; j <= n + line; j++) {
-    let data = input[j].split(" ").map(Number);
-    arr.push(data);
+  for (let j = idx + 1; j <= idx + n; j++) {
+    let [s, t] = input[j].split(" ").map(Number);
+    arr.push([s, t]);
   }
-  arr.sort((a, b) => a[0] - b[0]); // 서류 등수 오름차순
+  // 서류 기준으로 정렬 (1등인 사람은 무조건 붙음)
+  arr.sort((a, b) => a[0] - b[0]);
 
-  let minValue = Number.MAX_SAFE_INTEGER;
-  let ans = 0;
-  for (let [x, y] of arr) {
-    if (y < minValue) {
-      ans++;
-      minValue = y;
+  let inter = arr[0][1];
+  let cnt = 1;
+  for (let k = 1; k < n; k++) {
+    if (arr[k][1] < inter) { // 현재 사람보다 면접 등수가 더 높다면 3<4
+      inter = arr[k][1]; // 그 사람이 기준이 됨
+      cnt++;
     }
   }
-  console.log(ans);
-  line += n + 1;
+  console.log(cnt);
+  idx += n + 1;
 }
