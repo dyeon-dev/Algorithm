7ad42d0e91556a7f7b92a6c2b19c4efcae5f66a2
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-05-22 12:04:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 60.06 ms, Memory: 48.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,23 +1,25 @@
 function solution(n, edge) {
-    let graph = Array.from({length: n+1}, ()=>[]);
+    let graph = Array.from({ length: n+1 }, ()=>[]);
     // 양방향 그래프
     for([a, b] of edge) {
         graph[a].push(b);
         graph[b].push(a);
     }
-    let dist = Array(n+1).fill(0);
-    let q = [1];
+    let q=[1];
+    let dist=Array(n+1).fill(0);
     dist[1]=1;
-    while(q.length!=0) {
-        let now = q.shift();
-        for(x of graph[now]) {
+    while(q.length) {
+        let n = q.shift();
+        let cnt=0;
+        for(x of graph[n]) {
             if(dist[x]==0) {
+                dist[x]=dist[n]+1;
                 q.push(x);
-                dist[x]=dist[now]+1;
             }
         }
     }
     dist.sort((a, b)=>b-a);
-    const answer = dist.filter((it)=>it==dist[0]);
+    let answer = dist.filter((x)=> x==dist[0])
     return answer.length;
 }
+
