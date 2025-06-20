5b3d2c532d5e87a9c60b68177c6ccc2f57809c3a
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2025-06-20 16:14:56 +0900

    [level 2] Title: 방문 길이, Time: 0.52 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,22 @@
+function solution(dirs) {
+    const direction = {
+        U: [0, 1],
+        D: [0, -1],
+        R: [1 ,0],
+        L: [-1, 0]
+    }
+    let visited = new Set();
+    let now=[0,0]
+    for(dir of dirs) {
+        let nx = now[0]+direction[dir][0]
+        let ny = now[1]+direction[dir][1]
+        
+        if(nx>5 || ny>5 || nx<-5 || ny<-5) continue;
+        
+        visited.add(`${now[0]}${now[1]}${nx}${ny}`)
+        visited.add(`${nx}${ny}${now[0]}${now[1]}`)
+        
+        now=[nx, ny]
+    }
+    return visited.size/2
+}
