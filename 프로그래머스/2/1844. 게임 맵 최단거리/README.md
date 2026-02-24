8eeab2ecb0eacfd6b67e63be0eb8ef82b43b1f22
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-02-24 18:13:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 38.02 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 37.7 MB, 시간: 36.53 ms
+메모리: 37.9 MB, 시간: 38.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 13:46:19
+2026년 02월 24일 18:13:55
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,42 +1,29 @@
-class Queue {
-    constructor() {
-        this.q=[];
-        this.head=0;
-        this.tail=0;
-    }
-    enqueue(value) {
-        this.q[this.tail++] = value;
-    }
-    dequeue() {
-        return this.q[this.head++];
-    }
-    isEmpty() {
-        return this.head==this.tail;
-    }
-}
 function solution(maps) {
-    let n=maps.length;
-    let m=maps[0].length;
-    let q=[];
+    const n=maps.length;
+    const m=maps[0].length;
+    const queue=[];
     
-    let dx=[0,0,1,-1];
-    let dy=[1,-1,0,0];
-    let visited = Array.from({length: n}, () => new Array(m).fill(false));
+    const dx=[0,0,1,-1];
+    const dy=[1,-1,0,0];
+   
+    const visited = Array.from({length: n}, () => Array(m).fill(false));
     
-    q.push([0, 0, 1]);
+    queue.push([0, 0, 1]);
     visited[0][0]=true;
+    let heap=0;
     
-    while(q.length>0) {
-        let [row, col, dist] = q.shift();
+    while(heap < queue.length) {
+        let [row, col, dist] = queue[heap++];
         
         if(row==n-1 && col==m-1) return dist;
+        
         for(let i=0; i<4; i++) {
             let nx=row+dx[i];
             let ny=col+dy[i];
             if(nx>=0 && ny>=0 && nx<n && ny<m) {
                 if(!visited[nx][ny] && maps[nx][ny]==1) {
                     visited[nx][ny]=true;
-                    q.push([nx, ny, dist+1]);
+                    queue.push([nx, ny, dist+1]);
                 }
             }
         }
