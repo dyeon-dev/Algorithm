0b40808bf543ff28bd2ee4adb9a8fc114563e7c8
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-03-03 14:40:24 +0900

    [Silver III] Title: N과 M (8), Time: 436 ms, Memory: 13652 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,32 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/);
+
+let idx = 0;
+const n = input[idx++];
+const m = input[idx++];
+const arr = []; // 조합을 계산하고자 하는 원소가 담긴 배열
+const selected = []; // 현재 조합에 포함된 원소 
+
+for(let i=0; i<n; i++) {
+    arr.push(input[idx++]);
+}
+
+arr.sort((a, b) => a-b);
+
+dfs(0, 0);
+
+function dfs(depth, start) {
+    if(depth==m) {
+        const result = [];
+        for(let i of selected) result.push(arr[i]);
+        console.log(result.join(" "));
+        return;
+    }
+    
+    for(let i=start; i<arr.length; i++) {
+        selected.push(i); 
+        // 핵심: i + 1이 아니라 i를 넘겨주어 현재 숫자 i를 다시 뽑을 수 있게 함
+        dfs(depth+1, i);
+        selected.pop();
+    }
+}
