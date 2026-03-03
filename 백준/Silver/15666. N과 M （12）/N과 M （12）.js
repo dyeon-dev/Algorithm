957afa787439cbb3d2080ce7903323e826139b73
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-03-03 14:57:08 +0900

    [Silver II] Title: N과 M (12), Time: 168 ms, Memory: 12188 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,31 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/);
+
+let idx = 0;
+const n = input[idx++];
+const m = input[idx++];
+const arr = []; // 조합을 계산하고자 하는 원소가 담긴 배열
+const selected = []; // 현재 조합에 포함된 원소 
+const result = new Set(); // 전역에서 모든 조합 결과 누적 선언
+
+for(let i=0; i<n; i++) {
+    arr.push(input[idx++]);
+}
+
+arr.sort((a, b) => a-b);
+
+dfs(0, 0);
+
+function dfs(depth, start) {
+    if(depth==m) {
+        result.add(selected.map(i => arr[i]).join(" "))
+        return;
+    }
+    
+    for(let i=start; i<arr.length; i++) {
+        selected.push(i);
+        dfs(depth+1, i);
+        selected.pop();
+    }
+}
+console.log([...result].join("\n"));
