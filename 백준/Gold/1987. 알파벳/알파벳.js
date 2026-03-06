c8a8b3792604a84449babf9f878089cd1ced6fc3
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-03-07 00:22:51 +0900

    [Gold IV] Title: 알파벳, Time: 7236 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -1,30 +1,36 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let [r, c] = input[0].split(" ").map(Number);
-let arr = [];
-for (let i = 1; i <= r; i++) arr.push(input[i]);
-let visited = new Set(); // 방문한 적 있는 알파벳 집합
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/);
 
-let max = 0;
-let dx = [0, 0, 1, -1];
-let dy = [1, -1, 0, 0];
+let idx = 0;
 
-function dfs(x, y, depth) {
-  max = Math.max(max, depth);
-
-  for (let i = 0; i < 4; i++) {
-    let nx = x + dx[i];
-    let ny = y + dy[i];
-    if (nx >= 0 && ny >= 0 && nx < r && ny < c && !visited.has(arr[nx][ny])) {
-      visited.add(arr[nx][ny]);
-      dfs(nx, ny, depth + 1);
-      visited.delete(arr[nx][ny]);
-    }
-  }
+const R = Number(input[idx++]);
+const C = Number(input[idx++]);
+const board = [];
+for(let i=0; i<R; i++) {
+    board.push(input[idx++].split(''));
 }
 
-// 좌측 상단부터 시작
-visited.add(arr[0][0]);
-dfs(0, 0, 1);
-console.log(max);
+let ans = 0;
+const visited = new Set();
+visited.add(board[0][0]);
 
+const dx = [0,0,1,-1];
+const dy = [1,-1,0,0];
+
+dfs(0, 0, 1);
+
+function dfs(x, y, depth) {
+    ans = Math.max(ans, depth);
+    
+    for(let i=0; i<4; i++) {
+        const nx = dx[i]+x;
+        const ny = dy[i]+y;
+        if(nx>=0 && ny>=0 && nx<R && ny<C && !visited.has(board[nx][ny])) {
+            visited.add(board[nx][ny]);
+            dfs(nx, ny, depth+1);
+            visited.delete(board[nx][ny]);
+        }
+    }
+}
+
+console.log(ans);
