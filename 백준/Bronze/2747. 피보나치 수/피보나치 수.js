8ec090ac94ce3b42189a0591d4c3d6b4084db199
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-03-09 16:21:03 +0900

    [Bronze II] Title: 피보나치 수, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.js
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.js
@@ -4,22 +4,15 @@ const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/
 let idx = 0;
 
 const N = Number(input[idx++]);
-const arr = [];
-for(let i=0; i<N; i++) {
-    arr.push(Number(input[idx++]));
-}
+
 const dp = new Array(N+1).fill(-1);
 
 dp[0]=0;
 dp[1]=1;
 
-fibo(N);
-
-// 톱-다운 방식
-function fibo(n) {
-    if(dp[n]!=-1) return dp[n];
-
-    return dp[n] = fibo(n-2) + fibo(n-1);
+// 바텀-업 방식
+for(let i=2; i<=N; i++) {
+    dp[i] = dp[i-2] + dp[i-1];
 }
 
 console.log(dp[N]);
