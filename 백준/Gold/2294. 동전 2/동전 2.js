b64623064950e2d1758f683a7d4c9c6d6a375c3b
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-03-29 16:55:13 +0900

    [Gold V] Title: 동전 2, Time: 160 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.js
+++ 백준/Gold/2294. 동전 2/동전 2.js
@@ -12,8 +12,10 @@ for(let i=0; i<N; i++) {
 const dp = new Array(K+1).fill(Infinity);
 dp[0]=0;
 
-for(let i=0; i<=N; i++) {
-    const c = coins[i-1];
+// 해당 방식은 루프 에러 발생 확률이 높다.
+// 그래서 for...of 문 사용하는 것이 더 깔끔하고 안전하다.
+for(let i=0; i<N; i++) {
+    const c = coins[i];
     for(let j=c; j<=K; j++) {
         dp[j]=Math.min(dp[j], dp[j-c]+1);
     }
