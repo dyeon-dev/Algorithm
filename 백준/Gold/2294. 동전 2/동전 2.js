0f9b606489709da7d5c6e5df2dea15595ab696b6
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-03-29 16:51:33 +0900

    [Gold V] Title: 동전 2, Time: 164 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.js
+++ 백준/Gold/2294. 동전 2/동전 2.js
@@ -9,25 +9,14 @@ const coins = [];
 for(let i=0; i<N; i++) {
     coins.push(Number(input[idx++]))
 }
-const dp = Array.from({ length: N+1 }, ()=>new Array(K+1).fill(Infinity));
+const dp = new Array(K+1).fill(Infinity);
+dp[0]=0;
 
-// 0원을 만드는 방법은 0개
 for(let i=0; i<=N; i++) {
-    dp[i][0]=0;
-}
-
-for(let i=1; i<=N; i++) {
     const c = coins[i-1];
-    for(let j=1; j<=K; j++) {
-        // 1. 일단 이전 동전까지만 사용해서 만든 최솟값을 그대로 가져옴
-        dp[i][j]=dp[i-1][j];
-        
-		// 2. 현재 동전(c)을 추가로 사용할 수 있다면 비교해서 갱신
-        if(j>=c) {
-            dp[i][j]=Math.min(dp[i][j-c]+1, dp[i][j]);
-        }
+    for(let j=c; j<=K; j++) {
+        dp[j]=Math.min(dp[j], dp[j-c]+1);
     }
 }
 
-const ans = dp[N][K];
-console.log(ans==Infinity?-1 : ans);
+console.log(dp[K]==Infinity?-1 : dp[K]);
