7cb4123dd8e7bab42d02a664006f661ddd66a7f4
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-04-13 16:53:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 236 ms, Memory: 27288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -3,25 +3,31 @@ const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/
 
 let idx = 0;
 
-const N = Number(input[idx++]); // 수빈
-const K = Number(input[idx++]); // 동생
+const N = Number(input[idx++]);
+const K = Number(input[idx++]);
 
-const visited = Array(1000001).fill(0);
+const visited = new Array(100001).fill(false);
 
-function bfs() {
-    const q = [N];
-    while(q.length) {
-        const cur = q.shift();
-        
-        if(cur==K) return visited[cur];
-        
-        for(let next of [cur-1, cur+1, cur*2]) {
-            if(visited[next]==0) {
-                visited[next]=visited[cur]+1;
-                q.push(next);
-            }
-        }
+function bfs(start) {
+  let q = [[start, 0]];
+  let head = 0;
+  visited[start]=true;
+
+  while(head<q.length) {
+    const [cur, cnt] = q[head++];
+
+    if(cur==K) {
+      return cnt;
     }
+
+    for(let n of [cur-1, cur+1, cur*2]) {
+      if(!visited[n] && n>=0 && n<100001) {
+        visited[n]=true;
+        q.push([n, cnt+1]);
+      }
+    }
+  }
+  return -1;
 }
 
-console.log(bfs());
+console.log(bfs(N));
