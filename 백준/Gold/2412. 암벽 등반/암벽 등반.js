fee4f6d41dd19572b3e42142074c3a210747eef5
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-04-13 17:14:19 +0900

    [Gold IV] Title: 암벽 등반, Time: 6824 ms, Memory: 32132 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.js
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.js
@@ -1,46 +1,47 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/).map(Number);
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/);
 
 let idx = 0;
-const n = input[idx++];
-const T = input[idx++];
-const position = [];
 
-for(let i=0; i<n; i++) {
-    const x = input[idx++];
-    const y = input[idx++];
-    position.push([x,y]);
+const N = Number(input[idx++]);
+const T = Number(input[idx++]);
+const arr = [];
+
+for(let i=0; i<N; i++) {
+  arr.push([Number(input[idx++]), Number(input[idx++])])
 }
 
-const pointSet = new Set();
-for(let i=0; i<n; i++) {
-    const [x, y] = position[i]
-    pointSet.add(`${x},${y}`)  // 문자열 키 생성
+arr.sort((a, b)=>a[1]!==b[1] ? a[1]-b[1] : a[0]-b[0]);
+
+const visited = new Array(N+1).fill(0);
+
+function bfs() {
+  let q = [[0, 0, 0]];
+  let head = 0;
+  visited[0][0]=true;
+
+  while(head<q.length) {
+    const [cx, cy, cnt] = q[head++]; // [현재 x, 현재 y, 이동횟수]
+
+    if(cy==T) {
+      return cnt;
+    }
+
+    for(let i=0; i<N; i++) {
+      if(!visited[i]) {
+        const [nx, ny] = arr[i];
+
+        if(ny > cy+2) break;
+        if(ny < cy-2) continue;
+
+        if(Math.abs(nx-cx)<=2) {
+          visited[i]=true;
+          q.push([nx, ny, cnt+1]);
+        }
+      }
+    }
+  }
+  return -1;
 }
 
 console.log(bfs());
-
-function bfs() {
-    const queue = [[0,0,0]];
-    let head=0;
-    
-    while(head<queue.length) {
-        const [cx, cy, cd] = queue[head++];
-        if(cy===T) return cd;
-         // 현재 위치에서 이동 가능한 모든 좌표(25개 조합)를 직접 생성
-        for(let nx=cx-2; nx<=cx+2; nx++) {
-            for(let ny=cy-2; ny<=cy+2; ny++) {
-                if(nx < 0 || ny < 0) continue;
-                
-                const key = `${nx},${ny}`;
-                // 해당 좌표에 홈이 있는지 O(1)로 확인
-                if(pointSet.has(key)) {
-                   // 밟은 홈은 리스트에서 즉시 제거 (방문 처리와 동일)
-                   pointSet.delete(key);
-                   queue.push([nx, ny, cd+1]);
-                }
-            }
-        }
-    }
-    return -1;
-}
