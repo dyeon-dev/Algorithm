e8618e8a1692bf2db3c94691c5ca5c42311e1002
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-04-30 11:13:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,7 +1,8 @@
 function solution(n) {
-    var answer = 0;
-    let dp = [1, 1]
-    for(let i=2; i<=n; i++) {
+    let dp = Array(n).fill(0);
+    dp[1]=1;
+    dp[2]=2;
+    for(let i=3; i<=n; i++) {
         dp[i]=(dp[i-1]+dp[i-2])%1234567
     }
     return dp[n];
