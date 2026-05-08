b355425e22f526f84ec038f01d4ec774fe47b83e
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-05-08 10:46:51 +0900

    Update 특정 거리의 도시 찾기.js

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.js
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.js
@@ -1,66 +1,48 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/);
 
-class Queue {
-  constructor() {
-    this.items = {};
-    this.headIndex = 0;
-    this.tailIndex = 0;
-  }
-  enqueue(item) {
-    this.items[this.tailIndex] = item;
-    this.tailIndex++;
-  }
-  dequeue() {
-    const item = this.items[this.headIndex];
-    delete this.items[this.headIndex];
-    this.headIndex++;
-    return item;
-  }
-  peek() {
-    return this.items[this.headIndex];
-  }
-  getLength() {
-    return this.tailIndex - this.headIndex;
-  }
+let idx = 0;
+const N = Number(input[idx++]); // 도시의 개수
+const M = Number(input[idx++]); // 도로의 개수
+const K = Number(input[idx++]); // 거리정보
+const X = Number(input[idx++]); // 출발도시 번호
+
+const graph = Array.from({ length: N+1 }. ()=>[]);
+
+for(let i=0; i < M; i++) {
+  let a = Number(input[idx++])
+  let b = Number(input[idx++])
+  graph[a].push(b);
 }
 
-// 도시 개수, 도로 개수, 거리 정보, 출발 도시 번호
-let [n, m, targetK, x] = input[0].split(" ").map(Number);
-let graph = [];
-for (let i = 1; i <= n; i++) {
-  graph[i] = [];
-}
-for (let i = 0; i < m; i++) {
-  let [x, y] = input[i + 1].split(" ").map(Number);
-  graph[x].push(y);
-}
+const visited = Array(N+1).fill(0);
 
-let data = [];
-let visited = new Array(n + 1).fill(-1);
-visited[x] = 0; // 출발 도시 방문
+bfs(X);
 
-let queue = new Queue();
-queue.enqueue([x, 0]);
+function bfs(start) {
+  let q = [start];
 
-while (queue.getLength() !== 0) {
-  let [now, dist] = queue.dequeue();
-
-  if (dist == targetK) data.push(now);
-
-  for (let next of graph[now]) {
-    if (visited[next] == -1) {
-      visited[next] = dist + 1;
-      queue.enqueue([next, dist + 1]);
+  while(q.length) {
+    let cur = q.shift();
+    for(let node of graph[cur]) { 
+      if(!visted[node]) {
+        visited[node]=visited[cur]+1;
+        q.push(node);
+      }
     }
   }
 }
 
-if (data.length == 0) {
+let answer = [];
+for(let i=0; i<visited.length; i++) {
+  if(visited[i]==K) {
+    answer.push(i)
+  }
+}
+
+if(answer.length==0) {
   console.log(-1);
 } else {
-  data.sort((a, b) => a - b);
-  for (x of data) {
-    console.log(x);
-  }
+  answer.sort((a, b)=>a-b);
+  console.log(answer.join('\n'));
 }
