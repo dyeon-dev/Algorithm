0c1c94d0e46c2a54d923d7af682ac89f323f67a1
DaYeonKim <93921784+dyeon-dev@users.noreply.github.com>
2026-07-02 16:08:14 +0900

    [Passed] 1부터 특정 수까지의 합 2 (141ms, 10MB)

---
+++ trail2/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(fact(n));
+        // Please write your code here.
+    }
+    public static int fact(int n) {
+        if(n==1) {
+            return 1;
+        }
+
+        return fact(n-1) + n;
+    }
+}
